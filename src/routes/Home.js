7bdd1d7ab06664aad404fcb78c04ec37230273cb
Ubinquitous <Ubinquitous1@gmail.com>
2022-07-31 11:20:47 +0000

    SETTING

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -37,7 +37,6 @@ const Home = ({ userObj }) => {
                     type="text" 
                     placeholder="What's on your mind?" 
                     maxLength={120} />
-
                 <input 
                     type="submit" 
                     value="mouse"
