136ee17063fc465c812008a8e8b595bbbbd5d047
Ubinquitous <ubinquitous1@gmail.com>
2022-09-17 14:22:51 +0900

    fix the Home : domain change

--- src/fbase.js
+++ src/fbase.js
@@ -4,17 +4,16 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxA8hDJ1yyFOIv1Uh_I__JHJdm6N8RAso",
-    authDomain: "anonymouse-b3f4b.firebaseapp.com",
-    projectId: "anonymouse-b3f4b",
-    storageBucket: "anonymouse-b3f4b.appspot.com",
-    messagingSenderId: "57725630452",
-    appId: "1:57725630452:web:c2566c3ca1353c21a66ba1"
-  };
-  
-firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBxA8hDJ1yyFOIv1Uh_I__JHJdm6N8RAso",
+  authDomain: "anonymouse-b3f4b.firebaseapp.com",
+  projectId: "anonymouse-b3f4b",
+  storageBucket: "anonymouse-b3f4b.appspot.com",
+  messagingSenderId: "57725630452",
+  appId: "1:57725630452:web:c2566c3ca1353c21a66ba1"
+};
 
-export const firebaseInstance = firebase;
+firebase.initializeApp(firebaseConfig);
 
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const firebaseInstance = firebase;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,7 +31,7 @@ const Home = ({ userObj, creatorAt }) => {
                 ))}
             </div>
         </div>
-    );
 
+    );
 }
 export default Home;
